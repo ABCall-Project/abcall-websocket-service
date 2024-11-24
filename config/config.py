f631b33106bab4a1fffeac74cde4dcc8c73c1625
zearkiatos <caprilespe@outlook.com>
2024-11-24 19:33:18 -0300

    Adding generative ia option

--- config/config.py
+++ config/config.py
@@ -1,17 +1,20 @@
 import os
 from dotenv import load_dotenv
 
-environment = os.getenv('FLASK_ENV')
-
-if environment == 'local':
-    load_dotenv(dotenv_path='.env.local')
-elif environment == 'test':
-    load_dotenv(dotenv_path='.env.test')
-else:
-    load_dotenv(dotenv_path='.env')
-
 class Config:
-    ENVIRONMENT = environment
-    APP_NAME=os.getenv('APP_NAME')
-    URL_REPORTS_SERVICE=os.getenv('URL_REPORTS_SERVICE')
-    URL_ISSUES_SERVICE= os.getenv('URL_ISSUES_SERVICE')
+    def __init__(self):
+        environment = os.getenv('FLASK_ENV')
+
+        print(environment)
+
+        if environment == 'local':
+            load_dotenv(dotenv_path='.env.local')
+        elif environment == 'test':
+            load_dotenv(dotenv_path='.env.test')
+        else:
+            load_dotenv(dotenv_path='.env')
+
+        self.ENVIRONMENT = environment
+        self.APP_NAME=os.getenv('APP_NAME')
+        self.URL_REPORTS_SERVICE=os.getenv('URL_REPORTS_SERVICE')
+        self.URL_ISSUES_SERVICE= os.getenv('URL_ISSUES_SERVICE')
